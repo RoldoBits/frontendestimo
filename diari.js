0405cfe4f3740875ae5621673eea69ef81fe1b77
tonlls <tonlls1999@gmail.com>
2021-12-12 11:58:26 +0100

    2

--- diari.js
+++ diari.js
@@ -14,7 +14,7 @@ fetch('http://localhost:8000/user/get_journal'+TOKEN,{
     });
 function createElements(data){
     var main=document.getElementById("main_div");
-    
+    main.innerHTML="";
     for (let [key, value] of Object.entries(data)) {
         var para = document.createElement("P");               // Create a <p>   element
         para.style.border="1px solid black";
@@ -22,4 +22,39 @@ function createElements(data){
         para.innerText = key+' - '+value.data;               // Insert text
         main.appendChild(para);  
     }
+}
+function save(){
+    var main=document.getElementById("data");
+    var data={};
+    for (let [key, value] of Object.entries(main.children)) {
+        data[key]=value.innerText;
+    }
+    console.log(data);
+    fetch('http://localhost:8000/user/add_to_journal'+TOKEN+'&data='+main.value,{
+        method: 'POST',
+        // body: JSON.stringify({token:TOKEN,data:data}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then((response) => response.json())
+    .then((responseJSON) => {
+        // do stuff with responseJSON here...
+        console.log(responseJSON);
+        document.getElementById('id01').style.display='none'
+        main.value="";
+        fetch('http://localhost:8000/user/get_journal'+TOKEN,{
+        method: 'GET',
+        // body: JSON.stringify({token:TOKEN}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then((response) => response.json())
+    .then((responseJSON) => {
+    // do stuff with responseJSON here...
+        console.log(responseJSON);
+        createElements(responseJSON);
+        // window.location="/home.html?token="+TOKEN;
+    });
+        // window.location="/home.html?token="+TOKE N;
+    });
 }
